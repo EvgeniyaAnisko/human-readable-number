3ea145fcf443203422a73175852793562beb0688
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-01-16 14:25:48 +0300

    feat: add function human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let temp = ''
+    const one_ = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const ten_ = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    const ten_nineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    if (number === 0) {
+        return one_[0]
+    }
+
+    if (Math.trunc(number / 100)) {
+        temp = one_[Math.trunc(number / 100)] + ' hundred'
+    }
+
+    if ((number % 100 < 20) && (number % 100 > 9)) {
+        if (temp != '')
+            temp = temp + ' '
+        return temp + ten_nineteen[number % 10]
+    }
+
+    if (number % 100 > 10) {
+        if (temp != '') {
+            temp = temp + ' '
+        }
+        temp = temp + ten_[(number % 100 - number % 10) / 10]
+
+    }
+
+    if (number % 10) {
+        if (temp != '') {
+            temp = temp + ' '
+        }
+        temp = temp + one_[number % 10]
+    }
+    return temp;
 }
